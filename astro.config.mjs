99beaaac83cfe8b3e049bab5c1f782eb262613b1
André Carvalhal <andre-c_rj@hotmail.com>
2026-08-09 05:12:57 -0300

    Cloudflare Static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    // Garante que o build gere apenas HTML/CSS/JS estáticos puros
     output: 'static',
-
-    // Força o Astro a usar a data que o servidor da Cloudflare aceita
     compatibilityDate: '2026-08-08',
+
+    // 🚀 FORÇA O ASTRO A PROCESSAR AS IMAGENS VIA NODE, SEM ENVOLVER WORKERS
+    image: {
+        service: {
+            entrypoint: 'astro/assets/services/sharp'
+        }
+    }
 });
