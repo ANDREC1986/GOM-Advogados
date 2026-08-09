1ba1b52c49b84b817e3b55bb7b4f186e90573881
André Carvalhal <andre-c_rj@hotmail.com>
2026-08-09 05:20:48 -0300

    xx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     output: 'static',
-    compatibilityDate: '2026-08-08',
-
-    // 🚀 FORÇA O ASTRO A PROCESSAR AS IMAGENS VIA NODE, SEM ENVOLVER WORKERS
     image: {
         service: {
             entrypoint: 'astro/assets/services/sharp'
